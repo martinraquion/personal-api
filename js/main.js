76d1027c6b464123bd647b7cbcf0cbd8687f36fa
Martin Raquion <martin@boom.camp>
2019-06-12 00:23:25 +0800

    Tried integrating itunes api to my app

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     const allowCORS = `https://cors-anywhere.herokuapp.com/`
     let country ='us'
     let musixUrl = `${allowCORS}https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=100&country=${country}&f_has_lyrics=1@&apikey=${apikey}`
-    let itunesUrl = `${allowCORS}https://itunes.apple.com/search?term=bad+guy`;
+    let itunesUrl = `${allowCORS}https://itunes.apple.com/search?term=`;
     
     console.log(musixUrl);
     
@@ -12,23 +12,52 @@ $( document ).ready(function() {
     // })
     
     // console.log(url)
+
+
+    // write a function for getting album art and preview song from itunes api
+
+  
+      fetch(`${allowCORS}https://itunes.apple.com/search?term=bad+guy`)
+        .then(res => res.json())
+        .then(function(data){
+          let artprev = data.results[0].artworkUrl100;
+          `<img src="${artprev}" class="album-art" height="100px" width="100px">`
+          // console.log(artprev)
+        })
     
+
+    // console.log(getArtPrev())
+
     
     // QUERYING TOP SONGS
     fetch(musixUrl)
         .then(res => res.json())
         .then(function(chart){
             const topSongs = chart.message.body.track_list;
-            
-            for(let x in topSongs){
-                   
+            // var replaced = str.split(' ').join('+');
+           
+              //  albumidURL = ${topSongs[x].track.track_name}
+            //  let str;
+              fetch(`${allowCORS}https://itunes.apple.com/search?term=bad+guy`)
+                .then(res => res.json())
+                .then(function(data){
+                  let artprev = data.results[0].artworkUrl100;
+                  let songprev = data.results[0].previewUrl
+                  let artString = JSON.stringify(artprev);
+                  
+                  // let str = artString;                 
+                  
+                  // console.log(artString)
+               
+              for(let x in topSongs){
+
                 let rank = Number(x)+1;
                 $('.rank-container').append(`
                 <div class="rank-box">
-              <div class="rank-info">
+                <div class="rank-info">
                 <span class="rank-num">${rank}</span>
                 <div class="album-art-box">
-                  <img src="img/album-art.jpg" class="album-art" height="100px" width="100px">
+                <img src=${artString} class="album-art" height="100px" width="100px">
                 </div>
                 <div class="song-info">
                     <span class="song-title">${topSongs[x].track.track_name}</span>
@@ -44,7 +73,10 @@ $( document ).ready(function() {
                
                 `)
                 // console.log(topSongs[x].track.track_name);
-            }
+                }
+              })
+            
+
         })
 
         // fetch(itunesUrl)
