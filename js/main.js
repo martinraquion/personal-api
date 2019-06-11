eac24567551b7327e2c703484d574085a669d423
Martin Raquion <raquionearl@gmail.com>
2019-06-11 17:24:17 +0800

    Initialize files for design

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$( document ).ready(function() {
+    const apikey = '1c8ab2ee6c9278c1787402e27c011570';
+    const allowCORS = `https://cors-anywhere.herokuapp.com/`
+    const url = `${allowCORS}https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=100&country=us&f_has_lyrics=1@&apikey=${apikey}`
+   
+    // $('.list').click(function(){
+    //     $('.list').append('<li>Earl</li>')
+    // })
+    
+    // console.log(url)
+    
+    
+    // QUERYING TOP SONGS
+    fetch(url)
+        .then(res => res.json())
+        .then(function(chart){
+            const topSongs = chart.message.body.track_list;
+            for(let x in topSongs){
+                let rank = Number(x)+1;
+                $('.rank-container').append(`
+                <div class="rank-box">
+              <div class="rank-info">
+                <span class="rank-num">${rank}</span>
+                <div class="album-art-box">
+                  <img src="img/album-art.jpg" class="album-art" height="100px" width="100px">
+                </div>
+                <div class="song-info">
+                    <span class="song-title">${topSongs[x].track.track_name}</span>
+                    <span class="song-artist">${topSongs[x].track.artist_name}</span>
+                    <span class="song-album">${topSongs[x].track.album_name}</span>
+                </div>
+                <div class="play-btn-box">
+                  <span class="fas fa-play-circle"></span>
+                </div>
+                
+              </div>
+            </div>
+               
+                `)
+                console.log(topSongs[x].track.track_name);
+            }
+        })
+});
