dc32ea0ff22110ab333100c6d13039bb774c8e58
Martin Raquion <raquionearl@gmail.com>
2019-06-12 15:42:06 +0800

    Added Chart Per Country Functionality and modified design

--- js/main.js
+++ js/main.js
@@ -1,18 +1,34 @@
 $( document ).ready(function() {
     const apikey = '1c8ab2ee6c9278c1787402e27c011570';
     const allowCORS = `https://cors-anywhere.herokuapp.com/`
-    let country ='us'
-    let musixUrl = `${allowCORS}https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=100&country=${country}&f_has_lyrics=1@&apikey=${apikey}`
+    let musixUrl = `${allowCORS}https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=100&country=us&f_has_lyrics=1@&apikey=${apikey}`
     let itunesUrl = `${allowCORS}https://itunes.apple.com/search?term=bad+guy`;
+    countriesURL = `https://restcountries.eu/rest/v2/all`
     
     console.log(musixUrl);
+
+    $('.countries-bar').hide()
+    $('.searchbox').hide()
+    $('#cur-country').hide()
+    // $('.info-bar').hide()  
+    // $('.sub-title').hide()
+
+    $('#song-search').click(function(){
+      $('.searchbox').slideToggle(200)
+    })
+
+    $('#globe').click(function(){
+      $('.countries-bar').slideToggle(100)
+    })
     
     // $('.list').click(function(){
     //     $('.list').append('<li>Earl</li>')
     // })
     
     // console.log(url)
-    
+    $('.rank-box').click(function(){
+      $('#info-bar').show()
+    })
     
     // QUERYING TOP SONGS
     fetch(musixUrl)
@@ -47,11 +63,78 @@ $( document ).ready(function() {
             }
         })
 
-        // fetch(itunesUrl)
-        // .then(res => res.json())
-        // .then(function(song){
-        //     let thesong = song.results[0].artworkUrl100;
-            
-        //     $('.album-art').attr('src', thesong.toString())
-        // })
+       
+
+        fetch(countriesURL)
+          .then(res => res.json())
+          .then(function(country){
+           
+            for(let x in country){
+              let countryCode = country[x].alpha2Code
+              $('.cb-container').append(`
+              <div class="cb-box" id ="${countryCode.toLowerCase()}">
+              <span class="flag-pic">
+              <img src="${country[x].flag}" alt="" width="44vw" height="30vw">
+              </span>
+              <div class="cb-name" id="${countryCode.toLowerCase()}">
+              ${country[x].alpha3Code}            
+              </div>
+            </div>
+              `)
+              // <option value='${countryCode.toLowerCase()}'>${country[x].alpha3Code}</option>
+             }
+
+
+             $('.cb-box').on('click', function(){
+              console.log('changed')
+              let newcountry = $(this).attr('id')
+              console.log(newcountry)
+
+             
+              $('.rank-container').html('')
+              let newmusixUrl = `${allowCORS}https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=100&country=${newcountry}&f_has_lyrics=1@&apikey=${apikey}`
+
+              fetch(newmusixUrl)
+              .then(res => res.json())
+              .then(function(chart){
+                  const topSongs = chart.message.body.track_list;
+                  
+                  for(let x in topSongs){
+                         
+                      let rank = Number(x)+1;
+                      $('.rank-container').append(`
+                      <div class="rank-box">
+                    <div class="rank-info">
+                      <span class="rank-num">${rank}</span>
+                      <div class="album-art-box">
+                        <img src="img/album-art.jpg" class="album-art" height="100px" width="100px">
+                      </div>
+                      <div class="song-info">
+                          <span class="song-title">${topSongs[x].track.track_name}</span>
+                          <span class="song-artist">${topSongs[x].track.artist_name}</span>
+                          <span class="song-album">${topSongs[x].track.album_name}</span>
+                      </div>
+                      <div class="play-btn-box">
+                        <span class="fas fa-play-circle"></span>
+                      </div>
+                      
+                    </div>
+                  </div>
+                     
+                      `)
+                  }
+              })
+
+             })
+             
+
+
+          })
+
+
+        
+
+
+         
+        
 });
